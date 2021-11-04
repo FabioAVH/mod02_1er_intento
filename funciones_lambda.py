0f7c5f13ab3eb585a23918fa1b88e01379a0b3d4
unknown <you@example.comfabio.vargas@opexa.co>
2021-11-03 21:00:49 -0500

    Actualizacion lambda

--- funciones_lambda.py
+++ funciones_lambda.py
@@ -1,9 +1,14 @@
 from funciones_primer_nivel import SumaTodos
 
-doble = lambda x: x*2 #lambda se usa para funciones anonimas
+doble = lambda x: x*2 #lambda se usa para funciones anonimas. Variable : codigo
 
 triple = lambda x: x*3
 
 print(SumaTodos(3, doble))
 
 print(SumaTodos(100, triple))
+
+'''
+Lo ideal seria:
+print(SumaTodos(3, lambda x: x*2))
+print(SumaTodos(100, lambda x: x*3))
