7d3dbfa69589daaeca020b9a4b91437562cbd887
unknown <you@example.comfabio.vargas@opexa.co>
2021-11-03 22:44:14 -0500

    Ejemplo de objetos - tortuga

---
+++ objetos.py
@@ -0,0 +1,11 @@
+import turtle
+
+tortuguita = turtle.Turtle() #estamos creando un objeto T
+otraTortuguita = turtle.Turtle() #segundo objeto
+
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+
+otraTortuguita.color('red')
+otraTortuguita.left(90)
+otraTortuguita.fd(50)
